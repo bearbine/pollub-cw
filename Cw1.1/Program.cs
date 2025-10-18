0a7c048b401a6632848baae72050ed24a0396b5d
romakozar2019@outlook.com <romakozar2019@outlook.com>
2025-10-18 22:04:02 +0200

    1.1

--- Cw1.1/Program.cs
+++ Cw1.1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Authentication.ExtendedProtection;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+         * 
+         * Zadanie 1.1. 
+Napisz program, który obliczy obwód i pole trójkąta o bokach 6,3, 6,7 oraz 7,2. 
+Do obliczenia pola zastosuj wzór Herona. Wyświetl otrzymane wyniki w konsoli z 
+dokładnością do dwóch miejsc po przecinku (:F2). 
+Wskazówka: do obliczenia pierwiastka kwadratowego użyj funkcji Math.Sqrt.
+      */
+
+        double a, b, c;
+
+        a = 6.3;
+        b = 6.7;
+        c = 7.2;
+
+        double obwod = a + b + c;
+        double p = 0.5 * obwod;
+        double pole = Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+        Console.WriteLine(obwod);
+        Console.WriteLine($"{pole}");
+
+    }
+}
